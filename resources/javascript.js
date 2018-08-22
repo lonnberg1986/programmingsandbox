788c32caa5a0070f75371616f4703342d5efb496
Jani Lönnberg <jani.lonnberg@protonmail.com>
2018-08-22 13:43:24 +0300

    Add changing welcome text

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,5 +1,15 @@
 window.onload = () => {
 
+    /* Welcome message */
+    let welcomeMessageElement = document.querySelector(".welcomeMessage");
+    let welcomeMessages = ["In Programming We Trust!", "The Code Is Strong In Here!"];
+    let selectedElement = Math.floor(Math.random()*welcomeMessages.length);
+    let title = "Programming Sandox - ";
+
+    welcomeMessageElement.textContent = welcomeMessages[selectedElement];
+    document.title = title + welcomeMessages[selectedElement];
+
+    /* JavaScript Clock */
     let clockElement = document.querySelector(".time");
     let currentTime = new Date();
 
@@ -12,4 +22,5 @@ window.onload = () => {
         clockElement.textContent = `${currentTime.getHours() < 10 ? "0" + currentTime.getHours() : currentTime.getHours()}:${currentTime.getMinutes() < 10 ? "0" + currentTime.getMinutes(): currentTime.getMinutes()}:${currentTime.getSeconds() < 10 ? "0" + currentTime.getSeconds() : currentTime.getSeconds()}`;
         setTimeout(updateTime, 1000);
     }
+
 };
